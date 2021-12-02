a55d974c9fe868ec270d2457adfbe73e8a65ad5a
Martin Hromada <hromada.martin@seznam.cz>
2021-12-02 14:11:43 +0100

    Version 2

--- Game/Bulls_Cows.py
+++ Game/Bulls_Cows.py
@@ -1,23 +1,30 @@
 def main():
     date = datetime.date.today()
+    sep = len(TEXTS[4]) * "-"
+
     width, file_sep = file_sep_width(HEADER, date)
 
-    sep = len(TEXTS[1]) * "-"
-    prompt_print(TEXTS, sep)
+    prompt_print1(TEXTS, sep, GAME_CHOOSE)
+
+    difficulty = game_difficulty()
+
+    num_quantity = int(GAME_CHOOSE[difficulty][0])
+
+    prompt_print2(TEXTS, sep, GAME_CHOOSE, difficulty)
 
     if not os.path.isfile(FILE_PATH):
         create_file(HEADER, FILE_PATH, width, file_sep)
 
-    guessed_nums = random_nums()
+    guessed_nums = random_nums(num_quantity)
     guesses = 0
     start_time = time()
 
     while True:
         guesses += 1
-        player = player_choice()
+        player = player_choice(num_quantity)
         result = bull_cow_conditions(player, guessed_nums)
 
-        if win_condition(result, guesses):
+        if win_condition(result, guesses, num_quantity):
             result_evaluation(guesses, EVALUATION)
             break
         else:
@@ -40,16 +47,40 @@ def file_sep_width(head: tuple, dat) -> list:
     return new_list
 
 
-def prompt_print(text: tuple, sep: str) -> None:
-    print(f"{text[0]:^{len(sep)}}\n{sep}\n{text[1]}\n{text[2]}\n{sep}\n"
-          f"{text[3]}\n{sep}")
+def prompt_print1(text: tuple, sep: str,
+                  game_choose: tuple) -> None:
+    print(f"{text[0]:^{len(sep)}}\n{sep}\n{text[4]}")
+
+    for i, n in enumerate(game_choose):
+        print(f"{i+1}| {n}")
+
+    print(sep)
 
 
-def random_nums() -> dict:
+def prompt_print2(text: tuple, sep: str,
+                  game_choose: tuple, difficulty: int) -> None:
+    print(f"{sep}\n{text[1].format(game_choose[difficulty][0])}\n{text[2]}")
+    print(f"{sep}\n{text[3]}\n{sep}")
+
+
+def game_difficulty() -> int:
+    while True:
+        choose = input("Choose your game:\n")
+
+        try:
+            num = int(choose)
+        except ValueError:
+            print("Warning...Wrong value!")
+            continue
+
+        return num-1
+
+
+def random_nums(num_quantity: int) -> dict:
     guessed_dict = {}
     value = 0
 
-    while not len(guessed_dict.keys()) == 4:
+    while not len(guessed_dict.keys()) == num_quantity:
         n = choice(range(0, 10))
 
         if n in guessed_dict.keys():
@@ -63,18 +94,18 @@ def random_nums() -> dict:
     return guessed_dict
 
 
-def player_choice() -> dict:
+def player_choice(num_quantity: int) -> dict:
     while True:
         player_input = input(">>>")
 
         if input_num_check(player_input):
             continue
-        elif input_len_check(player_input):
+        elif input_len_check(player_input, num_quantity):
             continue
         else:
             player_dic = {int(n): i for i, n in enumerate(player_input)}
 
-        if input_duplicate_check(player_dic):
+        if input_duplicate_check(player_dic, num_quantity):
             continue
         else:
             return player_dic
@@ -88,16 +119,16 @@ def input_num_check(player_input: str) -> bool:
     return False
 
 
-def input_len_check(player_input: str) -> bool:
-    if len(player_input) != 4:
-        print("Enter exactly 4 numbers! Try it again:")
+def input_len_check(player_input: str, num_quantity: int) -> bool:
+    if len(player_input) != num_quantity:
+        print("Enter exactly {} numbers! Try it again:".format(num_quantity))
         return True
     else:
         return False
 
 
-def input_duplicate_check(nums: dict) -> bool:
-    if len(nums) < 4:
+def input_duplicate_check(nums: dict, num_quantity: int) -> bool:
+    if len(nums) < num_quantity:
         print("Your numbers must be unique! Try it again:")
         return True
     else:
@@ -125,8 +156,8 @@ def bull_cow_conditions(player: dict, guessed_nums: dict) -> list:
     return result_list
 
 
-def win_condition(result: list, guesses: int) -> bool:
-    if result[0] == 4:
+def win_condition(result: list, guesses: int, num_quantity: int) -> bool:
+    if result[0] == num_quantity:
         if guesses == 1:
             print("Congratulation, you've guessed right number\nin 1 guess!")
             return True
@@ -138,10 +169,10 @@ def win_condition(result: list, guesses: int) -> bool:
         return False
 
 
-def result_evaluation(guesses: int, ev: dict) -> None:
-    for e in ev:
+def result_evaluation(guesses: int, evaluation: dict) -> None:
+    for e in evaluation:
         if guesses <= e:
-            print(f"That's {ev.get(e)}.")
+            print(f"That's {evaluation.get(e)}.")
             return None
     print("That's bad.")
 
@@ -167,9 +198,8 @@ def create_file(head: tuple, path: str, width: list, sep: str) -> None:
         print("File not found.")
 
 
-def print_to_file(
-        num: int, g_t: str, g_num: dict, dat,
-        path: str, width: list, sep: str) -> None:
+def print_to_file(num: int, g_t: str, g_num: dict, dat,
+                  path: str, width: list, sep: str) -> None:
     g_list = [str(n) for n in g_num]
     try:
         with open(path, "a") as file:
@@ -190,12 +220,13 @@ if __name__ == "__main__":
     from random import choice
     from time import time
 
-    TEXTS = "WELCOME TO BULLS & COWS.",\
-            "I've generated a random 4 digit number for you.",\
+    TEXTS = "Welcome to BULLS & COWS game",\
+            "I've generated a random {} digit number for you.",\
             "Let's play a bulls and cows game.",\
-            "ENTER NUMBERS:"
+            "ENTER NUMBERS:",\
+            "You can choose from three difficulties of the game:"
     HEADER = "Num of guesses", "Guess time", "Guess numbers", "Date"
     FILE_PATH = os.path.split(sys.argv[0])[0] + "/" + "results.txt"
     EVALUATION = {10: "amazing", 20: "average", 30: "not so good"}
-
+    GAME_CHOOSE = "3-Numbers", "4-Numbers", "5-Numbers"
     main()
